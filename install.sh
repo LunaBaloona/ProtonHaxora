799d382a6f77c3fb46f3a04d056cf51485dc7981
LunaBaloona <johan@sunwell.ie>
2026-01-01 17:51:10 +0000

    Initial release: QT/KDE compatible Autodiscover game

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "Building Protonhax GUI..."
+cargo build --release
+mkdir -p ~/.local/bin
+cp target/release/protonhax-gui ~/.local/bin/
+echo "Installation complete! You can now run 'protonhax-gui' or find it in your menu."
